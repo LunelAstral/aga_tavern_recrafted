fef4d4b9a73d3067f4a388c62d9128fdfaa53b84
Rile_Cry <52363024+Rile-Cry@users.noreply.github.com>
2025-05-06 14:54:45 -0500

    Initial commit

---
+++ utility/helpers/file_helper.gd
@@ -0,0 +1,17 @@
+class_name FileHelper
+
+static func load_asset(path : String) -> Resource:
+	if OS.has_feature("export"):
+		# Check if file is .remap
+		if not path.ends_with(".remap"):
+			return load(path)
+		
+		# Open the file
+		var __config_file = ConfigFile.new()
+		__config_file.load(path)
+		# Load the remapped file
+		var __remapped_file_path = __config_file.get_value("remap", "path")
+		__config_file = null
+		return load(__remapped_file_path)
+	else:
+		return load(path)
