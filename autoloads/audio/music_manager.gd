15d680f9d7f69d095aebc8509bb455c19a1cf286
Rile-Cry <rileycriddle@yahoo.com>
2025-08-12 14:45:32 -0400

    MusicManager update
    Changed how the MusicManager loads osts, next push will be the same for the SoundManager.

--- autoloads/audio/music_manager.gd
+++ autoloads/audio/music_manager.gd
@@ -1,10 +1,13 @@
+## The Singleton in charge of managing the Music of the game
 extends Node
 
-#region Variable Definitions
-var song_pool : Dictionary[StringName, Array]
+#region Declarations
+@export var song_pool : Array[OstHolder]
+
 var main_music_player : AudioStreamPlayer
 var secondary_music_player : AudioStreamPlayer
-var current_song : StringName
+var current_song : OstHolder
+var current_time : float
 var crossfade := false
 #endregion
 
@@ -13,15 +16,21 @@ func _ready() -> void:
 	setup_music_players()
 
 func _process(delta: float) -> void:
-	if current_song != &"":
-		if song_pool.get(current_song).get(1) == 0:
+	if current_song:
+		if not current_song.can_loop:
+			return
+		
+		if current_song.loop_vector.y == -1:
 			var current_pos = main_music_player.get_playback_position()
-			if current_pos == 0.0:
-				play_song(current_song)
-		elif song_pool.get(current_song).get(1) != -1 and not crossfade:
+			if current_pos >= current_song.ost_stream.get_length():
+				play_song(current_song.ost_name, current_song.loop_vector.x)
+		elif current_song.loop_vector.y != -1 and not crossfade:
 			var current_pos = main_music_player.get_playback_position()
-			if current_pos >= song_pool.get(current_song).get(1):
-				play_song(current_song)
+			if current_pos >= current_song.loop_vector.y:
+				play_song(current_song.ost_name, current_song.loop_vector.x)
+
+func load_music() -> void:
+	pass
 
 func setup_music_players() -> void:
 	main_music_player = AudioStreamPlayer.new()
@@ -31,37 +40,28 @@ func setup_music_players() -> void:
 	secondary_music_player = AudioStreamPlayer.new()
 	secondary_music_player.bus = GenumHelper.BusName.get(Genum.BusID.OST)
 	add_child(secondary_music_player)
-
-func add_ost(stream: AudioStream, loop: int) -> void:
-	print("added %s" % stream.resource_name)
-	song_pool.set(stream.resource_name, [stream, loop])
-
-func remove_ost(stream_name: StringName) -> void:
-	if song_pool.has(stream_name):
-		song_pool.erase(stream_name)
-	else:
-		push_warning("There is no stream by %s in song_pool" % stream_name)
 #endregion
 
 #region Usage Functions
-func play_song(song_name: StringName, crossover: bool = false) -> void:
+func play_song(song_name: StringName, play_position: float = 0, crossover: bool = false) -> void:
 	# TODO: Need to be able to loop the song as Yuvi describes, might need to change
 	# how songs are loaded?
-	if not song_pool.has(song_name):
+	current_song = _find_song(song_name)
+	if not current_song:
 		push_error("There's no song by the name %s in song_pool" % song_name)
+		return
 	
-	current_song = song_name
 	if crossover:
 		_crossover(song_name)
 	else:
-		main_music_player.stream = song_pool.get(song_name).get(0)
+		main_music_player.stream = current_song.ost_stream
 		main_music_player.play()
 
 func _crossover(song_name: StringName, sfx_crossover := false) -> void:
 	crossfade = true
 	if !sfx_crossover:
 		# Setup new song on secondary
-		secondary_music_player.stream = song_pool.get(song_name).get(0)
+		secondary_music_player.stream = _find_song(song_name).ost_stream
 		secondary_music_player.volume_linear = 0
 		secondary_music_player.play()
 		
@@ -98,5 +98,14 @@ func play_jingle(sfx: StringName) -> void:
 
 func stop_song() -> void:
 	main_music_player.stop()
-	current_song = &""
+	current_song = null
+#endregion
+
+#region Helper Methods
+func _find_song(ost_name: StringName) -> OstHolder:
+	for ost in song_pool:
+		if ost.ost_name == ost_name:
+			return ost
+	
+	return null
 #endregion
